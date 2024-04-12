8187a9ffe56939f37e0e6c1816a9b819d3451e57
Kunato Nishina <nishina-kunato723@g.ecc.u-tokyo.ac.jp>
2024-04-12 16:25:28 +0900

    Fix the case generator to match the terms in the paper

--- CaseGenerator.py
+++ CaseGenerator.py
@@ -1,13 +1,16 @@
-import unicodedata
+from glob import glob
 import os
 import random
+import unicodedata
 import xml.etree.ElementTree as ET
-from glob import glob
 
 COLORS = {"red" : "#FF0000", "green" : "#00FF00", "blue" : "#0000FF", "yellow" : "#FFFF00", "cyan" : "#00FFFF", "magenta" : "#FF00FF", "white" : "#FFFFFF", "black" : "#000000"}
 ET.register_namespace("", "http://www.w3.org/2000/svg")
 
 def get_existing_colors(original: str) -> tuple[set, ET.Element]:
+  '''
+  Lists all the colors set in the `fill` attribute of the input SVG code as a set together with the root element of the SVG.
+  '''
   S = set()
 
   root = ET.fromstring(original)
@@ -18,6 +21,9 @@ def get_existing_colors(original: str) -> tuple[set, ET.Element]:
   return S, root
 
 def output_to_file(n: str, output_folder: str, original: str, query: str, answer: str):
+  '''
+  Outputs the prompt and the answer image to the appropriate path.
+  '''
   os.makedirs(f"{output_folder}/query/", exist_ok=True)
   os.makedirs(f"{output_folder}/answer/", exist_ok=True)
   with open(f"{output_folder}/query/{n}.txt", "w") as q:
@@ -26,6 +32,9 @@ def output_to_file(n: str, output_folder: str, original: str, query: str, answer
     a.write(answer)
 
 def change_color(original: str, n: str, name: str):
+  '''
+  Generates cases for the Change Color task.
+  '''
   S, _ = get_existing_colors(original)
   color_from = random.choice(list(S))
   color_to = random.choice(list(COLORS))
@@ -33,6 +42,9 @@ def change_color(original: str, n: str, name: str):
   output_to_file(n, "cases/1_ChangeColor", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please generate an SVG code that changes the part of the emoji with a {color_from} color to {color_to}.", original_content.replace(color_from, color_to))
 
 def set_contour(original: str, n: str, name: str, width: int):
+  '''
+  Generates cases for the Set Contour task.
+  '''
   S, root = get_existing_colors(original)
   area_color = random.choice(list(S))
 
@@ -42,10 +54,16 @@ def set_contour(original: str, n: str, name: str, width: int):
       e.attrib["stroke-width"] = str(width)
   output_to_file(n, "cases/2_SetContour", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please generate an SVG code that draws a black line around the part of the emoji with a {area_color} color.", ET.tostring(root, encoding='unicode'))
 
-def compact_representation(original: str, n: str, name: str):
-  output_to_file(n, "cases/3_CompactRepresentation", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please generate a more compact SVG code that represents the same emoji.", original)
+def compression(original: str, n: str, name: str):
+  '''
+  Generates cases for the Compression task. Note that the answer image is the same as the original.
+  '''
+  output_to_file(n, "cases/3_Compression", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please generate a more compact SVG code that represents the same emoji.", original)
 
 def up_side_down(original: str, n: str, name: str):
+  '''
+  Generates cases for the Upside-Down task.
+  '''
   _, root = get_existing_colors(original)
 
   assert root.attrib["viewBox"] == "0 0 36 36"
@@ -53,18 +71,24 @@ def up_side_down(original: str, n: str, name: str):
   root.attrib["transform"] = "translate(0,36) scale(1,-1)"
   output_to_file(n, "cases/4_UpSideDown", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please flip this emoji upside down.", ET.tostring(root, encoding='unicode'))
 
-def add_transparency(original: str, n: str, name: str):
+def transparency(original: str, n: str, name: str):
+  '''
+  Generates cases for the Transparency task.
+  '''
   _, root = get_existing_colors(original)
 
   root.attrib["opacity"] = "0.5"
-  output_to_file(n, "cases/5_AddTransparency", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please make this emoji transparent by half.", ET.tostring(root, encoding='unicode'))
+  output_to_file(n, "cases/5_Transparency", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please make this emoji transparent by half.", ET.tostring(root, encoding='unicode'))
 
-def trim_by_half(original: str, n: str, name: str):
+def crop_to_half(original: str, n: str, name: str):
+  '''
+  Generates cases for the Crop to Half task.
+  '''
   _, root = get_existing_colors(original)
 
   assert root.attrib["viewBox"] == "0 0 36 36"
   root.attrib["viewBox"] = "0 0 18 36"
-  output_to_file(n, "cases/6_TrimByHalf", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please trim the right half and keep the left half.", ET.tostring(root, encoding='unicode'))
+  output_to_file(n, "cases/6_CropToHalf", original, f"The following code is the SVG code for the emoji '{name.lower()}'. Please trim the right half and keep the left half.", ET.tostring(root, encoding='unicode'))
 
 l = glob("twemoji/assets/svg/*.svg")
 random.shuffle(l)
@@ -90,10 +114,10 @@ for f in l:
 
   change_color(original_content, n, name)
   set_contour(original_content, n, name, 1)
-  compact_representation(original_content, n, name)
+  compression(original_content, n, name)
   up_side_down(original_content, n, name)
-  add_transparency(original_content, n, name)
-  trim_by_half(original_content, n, name)
+  transparency(original_content, n, name)
+  crop_to_half(original_content, n, name)
 
   if count >= 100:
     break
